0263c7f209306214cc0b2f8421726d1a192e0905
thomas-gribling <tgriblin@student.42perpignan.fr>
2026-03-21 00:49:37 +0100

    removed printfs from show_alloc_mem

--- srcs/show_alloc_mem.c
+++ srcs/show_alloc_mem.c
@@ -1,54 +1,88 @@
 #include "malloc.h"
 
-void	ft_putstr(char *s)
-{
-	int	i = 0;
-
-	while (s[i]) {
+void	ft_putstr(char *s) {
+	int	i = -1;
+	while (s[++i])
 		write(0, &s[i], 1);
-		i++;
-	}
 }
 
-void	ft_putaddress(void *ptr) { // todo
-	unsigned long address = (unsigned long)(ptr);
-	printf("%lu\n", address);
+void	ft_putsize(size_t size) {
+	if (size > 9)
+		ft_putsize(size / 10);
+	char c = size % 10 + '0';
+	write(0, &c, 1);
+}
+
+void	ft_putaddress(unsigned long address, int first) {
+	if (first)
+		ft_putstr("0x");
+	if (address > 15)
+		ft_putaddress(address / 16, 0);
+		char c = "0123456789abcdef"[address % 16];
+	write(0, &c, 1);
 }
 
 void	show_alloc_mem() {
 	t_block *curr;
 	t_zone *curr2;
 
+	if (!z_tiny && !z_med && !z_big) {
+		ft_putstr("No memory allocated.\n");
+		return ;
+	}
+
 	curr = NULL;
 	if (z_tiny) {
-		printf("TINY : %p\n", z_tiny);
+		ft_putstr("TINY : ");
+		ft_putaddress((unsigned long)z_tiny, 1);
+		ft_putstr("\n");
 		curr = z_tiny->blocks;
 		while (curr) {
-			if (!curr->free)
-				printf("%p - %p : %ld bytes\n", curr, (curr + curr->size), curr->size);
+			if (!curr->free) {
+				ft_putaddress((unsigned long)curr, 1);
+				ft_putstr(" - ");
+				ft_putaddress((unsigned long)(curr + curr->size), 1);
+				ft_putstr(" : ");
+				ft_putsize(curr->size);
+				ft_putstr(" bytes\n");
+			}
 			curr = curr->next;
-		}
-	}
-	curr = NULL;
-	if (z_med) {
-		printf("MEDIUM : %p\n", z_med);
-		curr = z_med->blocks;
-		while (curr) {
-			if (!curr->free)
-				printf("%p - %p : %ld bytes\n", curr, (curr + curr->size), curr->size);
-			curr = curr->next;
-		}
-	}
-	curr2 = NULL;
-	if (z_big) {
-		printf("LARGE : %p\n", z_big);
-		curr2 = z_big;
-		while (curr2) {
-			printf("%p - %p : %ld bytes\n", curr2, (curr2 + curr2->size), curr2->size);
-			curr2 = curr2->next;
 		}
 	}
 
-	if (!z_tiny && !z_med && !z_big)
-		printf("No memory allocated.\n");
+	curr = NULL;
+	if (z_med) {
+		ft_putstr("MEDIUM : ");
+		ft_putaddress((unsigned long)z_med, 1);
+		ft_putstr("\n");
+		curr = z_med->blocks;
+		while (curr) {
+			if (!curr->free) {
+				ft_putaddress((unsigned long)curr, 1);
+				ft_putstr(" - ");
+				ft_putaddress((unsigned long)(curr + curr->size), 1);
+				ft_putstr(" : ");
+				ft_putsize(curr->size);
+				ft_putstr(" bytes\n");
+			}
+			curr = curr->next;
+		}
+	}
+
+	curr2 = NULL;
+	if (z_big) {
+		ft_putstr("LARGE : ");
+		ft_putaddress((unsigned long)z_big, 1);
+		ft_putstr("\n");
+		curr2 = z_big;
+		while (curr2) {
+			ft_putaddress((unsigned long)curr2, 1);
+			ft_putstr(" - ");
+			ft_putaddress((unsigned long)(curr2 + curr2->size), 1);
+			ft_putstr(" : ");
+			ft_putsize(curr2->size);
+			ft_putstr(" bytes\n");
+			curr2 = curr2->next;
+		}
+	}
 }
